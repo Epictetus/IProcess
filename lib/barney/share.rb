040322e5b6f60598f6d41518b27d020245edcfcf
robgleeson <rob@flowof.info>
2011-04-20 03:46:50 +0100

    Revamp the API documentation.

--- lib/barney/share.rb
+++ lib/barney/share.rb
@@ -14,25 +14,32 @@ module Barney
     @mutex = Mutex.new
 
     class << self
-      # Returns the latest value read from a spawned child process. 
+      # Returns the last value read from a spawned child process.
+      #
       # @api private
       # @return [Object]
       attr_accessor :value
 
-      # Returns a Mutex that is used when {Barney::Share.value Barney::Share.value} is being accessed by {Barney::Share#synchronize}
+      # Returns a Mutex that is used by the {Barney::Share#sync} method.
+      #
       # @api private
       # @return [Mutex] 
       attr_reader :mutex
     end
 
     # Returns a list of all variables or constants being shared for this instance of {Barney::Share Barney::Share}.
+    #
     # @return [Array<Symbol>] 
     attr_reader :variables
 
     # Returns the Process ID of the last forked process.
+    #
     # @return [Fixnum]
     attr_reader :pid
 
+    # Returns an Array of {HistoryItem} objects.
+    #
+    # @see HistoryItem
     # @return [Array<HistoryItem>]
     attr_reader :history
     
@@ -46,30 +53,29 @@ module Barney
       yield self if block_given? 
     end
 
-    # Serves as a method to mark a variable or constant to be shared between two processes. 
-    # @param  [Symbol] Variable   Accepts a variable amount of Symbol objects.
+    # Marks a variable or constant to be shared between two processes. 
+    #
+    # @param  [Symbol] Variable   Accepts the name(s) of the variables or constants you want to share.
     # @return [Array<Symbol>]     Returns a list of all variables that are being shared.
     def share *variables
       @variables.push *variables.map(&:to_sym)
       @variables.uniq!
     end
 
-    # Serves as a method to remove a variable or constant from being shared between two processes.
-    # @param  [Symbol] Variable Accepts a variable amount of Symbol objects.
+    # Removes a variable or constant from being shared between two processes.
+    #
+    # @param  [Symbol] Variable Accepts the name(s) of the variables or constants you want to stop sharing.
     # @return [Array<Symbol>]   Returns a list of the variables that are still being shared.
     def unshare *variables
       variables.map(&:to_sym).each { |variable| @variables.delete variable }
       @variables
     end
 
-    # Serves as a method to spawn a new child process.  
+    # Spawns a child process.  
     # It can be treated like the Kernel.fork method, but a block or Proc object is a required argument.
-    # @param  [Proc]  Proc    Accepts a block or Proc object that will be executed in a child 
-    #                         process.
-    # 
-    # @raise  [ArgumentError] It will raise an ArgumentError if a block or Proc object isn't 
-    #                         supplied as an argument. 
-    #                         
+    #
+    # @param  [Proc]  Proc    Accepts a block or Proc object that will be executed in a child process.
+    # @raise  [ArgumentError] Raises an ArgumentError if a block or Proc object isn't supplied.                        
     # @return [Fixnum]        Returns the Process ID(PID) of the spawned child process.  
     def fork &blk
       raise ArgumentError, "A block or Proc object is expected" unless block_given?
@@ -89,8 +95,9 @@ module Barney
       @pid
     end
 
-    # Serves as a method that synchronizes data between the parent and child process.  
-    # It will block until the spawned child process has exited. 
+    # Synchronizes data between the parent and child process.  
+    # It will block until the spawned child process has exited.
+    #
     # @return [void]
     def synchronize 
       Barney::Share.mutex.synchronize do
