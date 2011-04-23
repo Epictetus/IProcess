3e9ec1309f63ea6585941decbe54856312526392
Rob Gleeson <rob@flowof.info>
2011-04-23 04:41:10 +0100

    Proxy requests to Barney onto an instance of Barney::Share.

--- lib/barney.rb
+++ lib/barney.rb
@@ -1,6 +1,22 @@
 require 'barney/share' 
 
 module Barney 
+  
   VERSION = '0.9.0'
+
+  @proxy = Barney::Share.new
+
+  class << self
+
+    def method_missing meth, *args, &blk
+      if @proxy.respond_to? meth
+        @proxy.send meth, *args, &blk
+      else
+        super
+      end
+    end
+
+  end
+
 end
 
