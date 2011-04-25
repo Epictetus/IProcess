b6bfd6ec95cb999c0d6e510548dcdf2ffc7fba99
Rob Gleeson <rob@flowof.info>
2011-04-25 23:11:06 +0100

    Document Barney.respond_to?, Barney.method_missing …

--- lib/barney.rb
+++ lib/barney.rb
@@ -7,6 +7,8 @@ module Barney
 
   class << self
 
+    # Forward message to an instance of {Barney::Share Barney::Share}.
+    # @see Barney::Share Barney::Share.
     def method_missing meth, *args, &blk
       if @proxy.respond_to? meth
         @proxy.send meth, *args, &blk
@@ -15,6 +17,8 @@ module Barney
       end
     end
 
+    # Ask superclass or an instance of {Barney::Share Barney::Share} does it respond to _meth_.
+    # @return [Boolean] Return true if superclass or instance of {Barney::Share Barney::Share} responds to _meth_.
     def respond_to? meth, with_private = false
       super || @proxy.respond_to?(meth, with_private)
     end
