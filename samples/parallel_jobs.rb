df2adf32bb60e2ee3721b568785bdd004cac3147
Rob Gleeson <rob@flowof.info>
2011-05-03 12:18:11 +0100

    Rewrite "samples/parallel_jobs.rb" to use Barney.wait_all

--- samples/parallel_jobs.rb
+++ samples/parallel_jobs.rb
@@ -3,19 +3,15 @@ require 'barney'
 
 Barney.share :message
 message = ""
-pids    = []
 
 %w(r u b y).each do |letter|
-  pids << Barney.fork do 
+  Barney.fork do 
     message << letter
   end
 end
 
-pids.each do |pid| 
-  Process.wait pid 
-end
-
+Barney.wait_all
 Barney.sync
-message = Barney.history.map(&:value).join
 
+message = Barney.history.map(&:value).join
 p message # "ruby"
