b494c5b14a7cd8be0ee2593e5c14fda4031d5c4a
Rob Gleeson <rob@flowof.info>
2011-05-24 15:12:13 +0100

    Better test description.

--- test/suite/lib/barney/jobs.rb
+++ test/suite/lib/barney/jobs.rb
@@ -1,6 +1,6 @@
 describe '#Jobs' do
 
-  it 'should spawn workers, and return their return value in an array' do
+  it 'should spawn workers, and share their return value with the parent.' do
     result = Jobs(5) { 42 }
     assert_equal [42, 42, 42, 42, 42], result
   end
