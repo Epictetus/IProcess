2c94a7beba844679668f605cb7e40a47315d2ea6
Rob Gleeson <rob@flowof.info>
2011-05-24 15:47:37 +0100

    Document Jobs()

--- lib/barney/jobs.rb
+++ lib/barney/jobs.rb
@@ -1,3 +1,9 @@
+# Returns an Array populated by the return value of the block.  
+# Each block is executed in a subprocess.
+#
+# @param  [Proc]   Block      The block to execute in a subprocess. 
+# @param  [Fixnum] Processes  The number of subprocesses to spawn.
+# @return [Array<Object>] 
 def Jobs number, &block
   barney = Barney::Share.new
   barney.share :queue
