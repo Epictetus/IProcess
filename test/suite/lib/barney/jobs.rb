2c6daaa6e95a7b00778e80c9d65ebebca414a662
Rob Gleeson <rob@flowof.info>
2011-05-24 14:45:05 +0100

    describe "#Jobs".

--- test/suite/lib/barney/jobs.rb
+++ test/suite/lib/barney/jobs.rb
@@ -1,8 +1,8 @@
-describe Jobs do
+describe '#Jobs' do
 
   it 'should spawn workers, and return their return value in an array' do
     result = Jobs(5) { 42 }
-    assert_equal [42,42,42,42,42], result
+    assert_equal [42, 42, 42, 42, 42], result
   end
 
 
