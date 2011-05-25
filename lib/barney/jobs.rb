242c65413fe6fa5c2805325e4052104bc6e552f0
Rob Gleeson <rob@flowof.info>
2011-05-25 12:21:21 +0100

    Jobs() should raise an ArgumentError with no block.

--- lib/barney/jobs.rb
+++ lib/barney/jobs.rb
@@ -1,10 +1,17 @@
-# Returns an Array populated by the return value of the block.  
+# Returns an Array populated by the return value of a block.  
 # Each block is executed in a subprocess.
+#
+# @example
+#   results = Jobs(5) { 42 }
+#   p results # [42, 42, 42, 42, 42]
 #
 # @param  [Proc]   Block      The block to execute in a subprocess. 
 # @param  [Fixnum] Processes  The number of subprocesses to spawn.
+# @raise  [ArgumentError]     If block is missing.
 # @return [Array<Object>] 
 def Jobs number, &block
+  raise ArgumentError, 'block expected' unless block_given?
+
   barney = Barney::Share.new
   barney.share :queue
   queue = []
