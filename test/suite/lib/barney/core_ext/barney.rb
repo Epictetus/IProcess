41a71bdfb691dbd7bf647fd7718d7a59a0338199
Rob Gleeson <rob@flowof.info>
2011-06-13 19:21:20 +0100

    Add some tests.

--- test/suite/lib/barney/core_ext/barney.rb
+++ test/suite/lib/barney/core_ext/barney.rb
@@ -23,4 +23,35 @@ describe '#Barney' do
     assert_equal "baz", @foo
   end
 
+  it 'should evaluate the passed block in the context it is called in' do
+    klass = nil
+
+    Barney do
+      share :klass
+      fork { klass = self.class }
+    end
+
+    assert_equal klass, BarneySpec
+  end
+
+  it 'should define the methods of Barney::Share for the duration of its call.' do
+    Barney do
+      self.methods.each do |meth|
+        if Barney::Share.instance_methods(false).include? meth
+          assert_equal Barney::MethodLookup, method(meth).owner
+        end
+      end
+    end
+  end
+
+  it 'should undefine any methods it defines for the context of a single call.' do
+    Barney do
+      share :@foo
+      fork { @foo = "bar" }
+    end
+
+    Barney::Share.instance_methods(false).each do |meth|
+      assert_raises(NoMethodError, "#{meth} should be undefined!") { self.send(meth) }
+    end
+  end
 end
