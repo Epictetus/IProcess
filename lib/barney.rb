4e94234b735fda4a20ea75546f51980dc29afecd
Rob Gleeson <rob@flowof.info>
2011-06-13 18:47:25 +0100

    Remove deprecated methods from Barney::Share.

--- lib/barney.rb
+++ lib/barney.rb
@@ -7,18 +7,6 @@ require 'barney/core_ext/jobs'
 module Barney 
 
   VERSION = '0.15.1'
-  @proxy  = Barney::Share.new
-
-  class << self
-
-    Barney::Share.instance_methods(false).each do |method|
-      define_method method do |*args, &block|
-        $stderr.puts "[WARNING] Barney.#{method} is deprecated and will be removed in the next release."
-        @proxy.send method, *args, &block
-      end
-    end
-
-  end
 
 end
 
--- test/suite/lib/barney/core_ext/barney.rb
+++ test/suite/lib/barney/core_ext/barney.rb
@@ -1,14 +1,3 @@
-describe 'Barney' do
-
-  it 'should respond to all Barney::Share methods.' do
-    methods = Barney::Share.instance_methods false
-    methods.each do |meth|
-      assert Barney.respond_to?(meth.to_sym), "Barney should respond to #{meth}"
-    end
-  end
-
-end
-
 describe '#Barney' do
 
   it 'should synchronize a local variable.' do
