852693d4d39d3fbc93bb1c174c03dcf2f2ef8e02
Rob Gleeson <rob@flowof.info>
2011-11-02 01:06:32 +0000

    Rewrite the documentation in barney/share.rb

--- lib/barney/share.rb
+++ lib/barney/share.rb
@@ -1,30 +1,52 @@
 class Barney::Share
 
-  # @attr [Symbol] variable The name of the variable associated with _in_, and _out_.
-  # @attr [IO]     in       The pipe which is used to read data.       
-  # @attr [IO]     out      The pipe which is used to write data.
+  #
+  # @attr [Symbol] variable 
+  #   The name of the variable associated with _in_, and _out_.
+  #
+  # @attr [IO] in       
+  #   The pipe which is used to read data.  
+  #
+  # @attr [IO] out      
+  #   The pipe which is used to write data.
+  # 
   # @api private
+  # 
   StreamPair = Struct.new :variable, :in, :out
 
-  # @attr [Symbol] variable The variable name.
-  # @attr [Object] value    The value of the variable.
+  #
+  # @attr [Symbol] variable 
+  #   The variable name.
+  #
+  # @attr [Object] value    
+  #   The value of the variable.
+  #
   HistoryItem = Struct.new :variable, :value
 
-  # Returns a list of all variables or constants being shared for an instance of {Barney::Share}.
+  #
   # @return [Array<Symbol>] 
+  #   A list of variables being shared for a instance of {Barney::Share}.
+  #
   attr_reader :variables
 
-  # Returns the Process ID of the last forked process.
+  #
   # @return [Fixnum]
+  #   The Process ID of the last forked process.
+  #
   attr_reader :pid
 
-  # Returns an Array of {HistoryItem} objects.
+  #
   # @see HistoryItem
+  #
   # @return [Array<HistoryItem>]
+  #   An Array of {HistoryItem} objects.
+  #
   attr_reader :history
-  
-  # @yieldparam [Barney::Share] self Yields an instance of {Barney::Share}.
-  # @return [Barney::Share]
+ 
+  #
+  # @yieldparam [Barney::Share] _self 
+  #   An instance of {Barney::Share}.
+  #
   def initialize
     @streams   = []
     @variables = []
@@ -35,36 +57,58 @@ class Barney::Share
     yield self if block_given? 
   end
 
+  #
   # Marks a variable or constant to be shared between two processes. 
-  # @param  [Symbol, #to_sym]  Variable  Accepts the name(s) of the variables or constants you want to share.
-  # @return [Array<Symbol>]              Returns a list of all variables that are being shared.
+  #
+  # @param [#to_sym] variables  
+  #   Accepts the name(s) of the variables or constants you want to share.
+  #
+  # @return [Array<Symbol>] 
+  #   Returns a list of all variables that are being shared.
+  #
   def share *variables
     @variables.push(*variables.map(&:to_sym))
     @variables.uniq!
     @variables
   end
 
+  #
   # Removes a variable or constant from being shared between two processes.
-  # @param  [Symbol, #to_sym] Variable  Accepts the name(s) of the variables or constants you want to stop sharing.
-  # @return [Array<Symbol>]             Returns a list of the variables that are still being shared.
+  #
+  # @param [#to_sym] variables  
+  #   Accepts the name(s) of the variables or constants you want to stop sharing.
+  #
+  # @return [Array<Symbol>]             
+  #   Returns a list of the variables that are still being shared.
+  #
   def unshare *variables
     variables.each { |variable| @variables.delete variable.to_sym }
     @variables
   end
 
+  #
   # Collect the status of all subprocesses spawned by a {Barney::Share Barney::Share} instance.
+  #
   # @return [void]
+  #
   def wait_all
     @pids.each { |pid| Process.wait pid }
     @pids.clear
   end
 
+  #
   # Spawns a child process.  
   # It can be treated like the Kernel.fork method, but a block or Proc object is a required argument.
   # 
-  # @param  [Proc]  Proc    Accepts a block or Proc object that will be executed in a child process.
-  # @raise  [ArgumentError] Raises an ArgumentError if a block or Proc object isn't supplied.                        
-  # @return [Fixnum]        Returns the Process ID(PID) of the spawned child process.  
+  # @param [Proc] &block
+  #   A block or Proc object that will be executed in a child process.
+  #
+  # @raise [ArgumentError] 
+  #   Raises an ArgumentError if a block or Proc object isn't supplied. 
+  #
+  # @return [Fixnum]        
+  #   Returns the Process ID(PID) of the spawned child process. 
+  #
   def fork &block
     raise ArgumentError, "A block or Proc object is expected" unless block_given?
     @scope = block.binding
@@ -87,8 +131,11 @@ class Barney::Share
     @pid
   end
 
+  #
   # Synchronizes data between the parent and child process.  
+  #
   # @return [void]
+  #
   def synchronize 
     @streams.each do |stream|
       stream.out.close
