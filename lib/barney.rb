ad7a1f62e25cc7c44d64dd6ae80a78dc0d9c1e3c
Rob Gleeson <rob@flowof.info>
2011-11-09 05:10:37 +0000

    Enable Copy-On-Write friendly GC for REE.

--- lib/barney.rb
+++ lib/barney.rb
@@ -10,4 +10,7 @@ require 'barney/core_ext/barney'
 require 'barney/core_ext/jobs'
 require 'barney/version'
 
-
+# Ruby Enterprise Edition(REE).
+if GC.respond_to?(:copy_on_write_friendly=)
+  GC.copy_on_write_friendly = true
+end
