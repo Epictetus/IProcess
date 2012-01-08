b1686eb095e880a23e60cb72d1421a3af0753046
Rob Gleeson <rob@flowof.info>
2012-01-08 00:14:36 +0000

    Remove REE support (= 1.8)

--- lib/barney.rb
+++ lib/barney.rb
@@ -9,11 +9,3 @@ require 'barney/language'
 require 'barney/core_ext/barney'
 require 'barney/core_ext/jobs'
 require 'barney/version'
-
-#
-# Ruby Enterprise Edition (REE) only.
-# Enable CoW-friendly garbage collection.
-#
-if GC.respond_to?(:copy_on_write_friendly=)
-  GC.copy_on_write_friendly = true
-end
