ff6c71dd2a91d9fa8deba751aa980fa9b29e02c6
Rob Gleeson <rob@flowof.info>
2012-01-08 02:37:37 +0000

    Require 'barney/job', and 'barney/channel' in lib/barney.rb

--- lib/barney.rb
+++ lib/barney.rb
@@ -4,6 +4,8 @@ end
 require 'set'
 require 'mixit'
 require 'barney/streampair'
+require 'barney/channel'
+require 'barney/job'
 require 'barney/process'
 require 'barney/language'
 require 'barney/core_ext/barney'
