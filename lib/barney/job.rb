0b961cd481ed18b84739b683823e08fa82522356
Rob Gleeson <rob@flowof.info>
2012-01-08 02:37:09 +0000

    Add Barney::Job.

---
+++ lib/barney/job.rb
@@ -0,0 +1,70 @@
+class Barney::Job
+
+  #
+  # Spawn one or more jobs to be run in parallel.
+  #
+  # @param [Integer] number_of_jobs
+  #   The number of jobs to spawn.
+  #
+  # @param [Proc] worker
+  #   The unit of work to execute in one or more jobs.
+  #
+  # @return [Array<Object>]
+  #   The return value of one or more workers.
+  #
+  def self.spawn number_of_jobs = 1, &worker
+    jobs =
+    Array.new(number_of_jobs) do
+      job = Barney::Job.new(&worker)
+      job.execute
+      job
+    end
+
+    jobs.map do |job|
+      job.result
+    end
+  end
+
+  #
+  # @param [Proc] worker
+  #   The unit of work to execute in a subprocess.
+  #
+  # @raise [ArgumentError]
+  #   If a worker is not given.
+  #
+  # @return [Barney::Job]
+  #   Returns self.
+  #
+  def initialize &worker
+    unless block_given?
+      raise ArgumentError, 'No block given.'
+    end
+
+    @worker  = worker
+    @channel = nil
+    @pid     = nil
+  end
+
+  #
+  # Executes a unit of work in a subprocess.
+  #
+  # @return [Fixnum]
+  #   The process ID of the spawned subprocess.
+  #
+  def execute
+    @channel = Barney::Channel.new
+    @pid = fork { @channel.put(@worker.call) }
+  end
+
+  #
+  # @return [Object]
+  #   Returns the return value of the unit of work.
+  #
+  def result
+    Process.wait(@pid)
+    @channel.get
+  end
+
+end
+
+
