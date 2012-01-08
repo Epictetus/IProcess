6c0f4110c9d3c5084973343ef77379aa3b7f0431
Rob Gleeson <rob@flowof.info>
2012-01-08 02:36:56 +0000

    Add Barney::Channel.

---
+++ lib/barney/channel.rb
@@ -0,0 +1,41 @@
+class Barney::Channel
+
+  #
+  # @yieldparam [Barney::Channel] _self
+  #   Yields self.
+  #
+  def initialize
+    @reader, @writer = IO.pipe
+
+    if block_given?
+      yield self
+    end
+  end
+
+  #
+  # Add a object to the channel.
+  #
+  # @param [Object] object
+  #   A object to add to the channel.
+  #
+  def put object
+    @reader.close
+    @writer.write Marshal.dump(object)
+    @writer.close
+  end
+
+  #
+  # Get a object from the channel.
+  #
+  # @return [Object]
+  #   The object added to the channel.
+  #
+  def get
+    @writer.close
+    obj = Marshal.load(@reader.read)
+    @reader.close
+    obj
+  end
+
+end
+
