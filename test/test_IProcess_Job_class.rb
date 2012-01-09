3929b3dd27986b5f1aad4245b4241f7e1e14777b
Rob Gleeson <rob@flowof.info>
2012-01-09 07:12:43 +0000

    Refactor test.

--- test/test_IProcess_Job_class.rb
+++ test/test_IProcess_Job_class.rb
@@ -1,8 +1,8 @@
 context IProcess::Job do
   context 'spawn' do
-    it 'must spawn five workers and return the result of each.' do
-      topic = IProcess::Job.spawn(5) { :ok }
-      topic.must_equal([:ok, :ok, :ok, :ok, :ok])
+    it 'must spawn two workers and return the result of each.' do
+      topic = IProcess::Job.spawn(2) { :ok }
+      topic.must_equal([:ok, :ok])
     end
   end
 end
